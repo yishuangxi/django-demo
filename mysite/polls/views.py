fa7bfe25dd9923f04923964a5e52d9c3e0205aeb
yishuangxi <2546618112@qq.com>
2016-07-25 20:42:02 +0800

    hello detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("hello world")
+
+def detail(request):
+    return HttpResponse("Detail")
